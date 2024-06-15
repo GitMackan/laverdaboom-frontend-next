63fd51ff5dd09661e3f59daa3dfce58d7d10d927
Gitmackan <marcus@gameplan.com>
2024-06-16 00:29:29 +0200

    add env to next.config

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,7 @@ const nextConfig = {
 	env: {
 		INSTAGRAM_ACCESS_TOKEN:
 			"IGQWRQUmJaTVdSZA1NQX0hiQ0g4M2dwSzREUjVkOExGaTI0cXQ1WWVEb0JISE9oT3hLZAklObFNSbndNVHVhczhqWFNMVE5fdnNuTG1PZAmx3ZADZAHdjNKUGxaNWh3a25hdHhrQ3JCekFXQnVfeG9ITWpmeEFqbFcxSE0ZD",
+		SERVER_URL: "https://laverdaboom-api.herokuapp.com",
 	},
 };
 
