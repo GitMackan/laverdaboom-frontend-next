eefae0092d67550818f5cd0a28fe811c0f5f4932
Gitmackan <marcus@gameplan.com>
2024-06-16 20:49:53 +0200

    fix instagram access token

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	env: {
-		INSTAGRAM_ACCESS_TOKEN:
-			"IGQWRPTG9NUV9fOEs3TGtaczc3RXppUmtPbUpaemlNTkJCM19fUHdXZAXdnQm1BVjBoSHc3NkdDc2t1TG03Rkl6M1N5OWQ3X0xZARFJNMm5HOWJqRTF1TVBpbDV5NzdUZAVVFM1NEanJ5NHAyVDE3YzJHVlVaWFJUMHcZD",
 		SERVER_URL: "https://laverdaboom-api.herokuapp.com",
 	},
 };
